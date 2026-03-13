f4d85f25d6eb30356c34d2409c81818d094b8d2f
andreacb1995 <44872893+andreacb1995@users.noreply.github.com>
2026-03-13 11:17:28 +0000

    error vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // AÑADE ESTA LÍNEA AQUÍ ABAJO (Cambia TU_REPOSITORIO por el nombre real)
     base: '/https://github.com/andreacb1995/kuvu-landing-page.git/', 
     plugins: [react(), tailwindcss()],
     define: {
